0f8e3927e58e210f878b47b18801ce95edf326ef
IsiraAdithya <isiradabare1230@gmail.com>
2024-07-02 10:07:24 +0530

    First Commit

---
+++ Script/app.js
@@ -0,0 +1,10 @@
+customers{}
+
+function addCustomer() {
+
+    let name = document.getElementById("txtName").value;
+    let age = document.getElementById("txtAge").value;
+    let address = document.getElementById("txtAddress").value;
+
+    console.log(name);
+}
